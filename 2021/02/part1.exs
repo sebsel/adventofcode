cf9c4a5746b12ce1ec4fb84c59b081d14b38e572
Sebastiaan Andeweg <seb@seblog.nl>
2021-12-02 22:45:39 +0100

    2021 day 2

---
+++ 2021/02/part1.exs
@@ -0,0 +1,41 @@
+defmodule Answer do
+  defstruct [:horizontal, :depth, :answer]
+
+  def run() do
+    File.read!("./input.txt")
+    |> parse()
+    |> solve(0, 0)
+    |> IO.inspect()
+  end
+
+  def parse(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, " "))
+    |> Enum.flat_map(fn
+      [""] -> []
+      ["up", n] -> [{:up, n}]
+      ["down", n] -> [{:down, n}]
+      ["forward", n] -> [{:forward, n}]
+    end)
+    |> Enum.map(fn {dir, n} -> {dir, String.to_integer(n)} end)
+  end
+
+  def solve([], horizontal, depth) do
+    %Answer{horizontal: horizontal, depth: depth, answer: horizontal * depth}
+  end
+
+  def solve([{:up, n} | rest], horizontal, depth) do
+    solve(rest, horizontal, depth - n)
+  end
+
+  def solve([{:down, n} | rest], horizontal, depth) do
+    solve(rest, horizontal, depth + n)
+  end
+
+  def solve([{:forward, n} | rest], horizontal, depth) do
+    solve(rest, horizontal + n, depth)
+  end
+end
+
+Answer.run()
