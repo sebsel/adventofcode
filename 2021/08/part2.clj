70618bd630e04e95cc42550752d0ea85100444a1
Sebastiaan Andeweg <seb@seblog.nl>
2021-12-08 23:36:35 +0100

    2021 day 8: part 2 in Clojure
    Turns out I just had to learn about this one concept called 'let'
    and then it worked.

--- 2021/08/part2.clj
+++ 2021/08/part2.clj
@@ -16,50 +16,42 @@
 (defn first-superset [set2 coll] (first (filter #(clojure.set/superset? %1 set2) coll)))
 (defn first-not-superset [set2 coll] (first (filter #(not (clojure.set/superset? %1 set2)) coll)))
 
+
 (defn build-table [digits]
-  (def by-count (group-by count digits))
-  )
+  (let [by-count (group-by count digits)]
+    (let [one (first (get by-count 2))
+          four (first (get by-count 4))
+          seven (first (get by-count 3))
+          eight (first (get by-count 7))]
+      (let [nine (first-superset four (get by-count 6))
+            six (first-not-superset one (get by-count 6))
+            three (first-superset one (get by-count 5))]
+        (let [five (first-subset six (get by-count 5))]
+          (let [two (first (filter
+                             #(and
+                                (not= %1 three)
+                                (not= %1 five))
+                             (get by-count 5)))
+                zero (first (filter
+                             #(and
+                                (not= %1 six)
+                                (not= %1 nine))
+                             (get by-count 6)))
+                ]
+            {zero 0, one 1, two 2, three 3, four 4
+             five 5, six 6, seven 7, eight 8, nine 9}))))))
 
-  ; (def recipes {
-  ;   1 (fn [] (first (get by-count 2)))
-  ;   4 (fn [] (first (get by-count 4)))
-  ;   7 (fn [] (first (get by-count 3)))
-  ;   8 (fn [] (first (get by-count 7)))
-  ;   9 (fn [] (first-superset (first (get by-count 4)) (get by-count 6)))
-  ;   6 (fn [] (first-not-superset (first (get by-count 2)) (get by-count 6)))
-  ;   3 (fn [] (first-superset (first (get by-count 2)) (get by-count 5)))
-
-  ;   ; 5 (fn [table] (first-subset (get table 6) (get by-count 5)))
-  ;   ; 2 (fn [table] (first (filter
-  ;   ;                        #(and
-  ;   ;                           (not= %1 (get table 3))
-  ;   ;                           (not= %1 (get table 5)))
-  ;   ;                        (get by-count 5))))
-  ;   ; 0 (fn [table] (first (filter
-  ;   ;                        #(and
-  ;   ;                           (not= %1 (get table 6))
-  ;   ;                           (not= %1 (get table 6)))
-  ;   ;                        (get by-count 6))))
-  ;   })
-  ; (reduce (fn [table digit] (assoc table ((second digit)) (first digit))) {} recipes))
 
 (defn main [input]
-  (->> input (map (fn [line]
-                    (def table (build-table (first line)))
-                    (println table)
-                    (println (second line))
-                    (def digits (map #(get table %1) (second line))) digits))
-                    ; (+ (* 1000 (first digits))
-                    ;    (* 100 (nth digits 1))
-                    ;    (* 10 (nth digits 2))
-                    ;    (* 1 (nth digits 3))
-                    ;    )))
-       ; (apply +)
-       ))
-
-  ; (->> (map last input)
-       ; (map #(detect-number %1 (build-hash (map last input))))
-       ; (apply +)
-  ; ))
+  (->> input
+       (map (fn [line]
+              (let [table (build-table (first line))]
+                (let [digits (map #(get table %1) (second line))]
+                  (+ (* 1000 (first digits))
+                     (* 100 (nth digits 1))
+                     (* 10 (nth digits 2))
+                     (* 1 (nth digits 3))
+                     )))))
+            (apply +)))
 
 (print "Answer:" (main (parse-input "input.txt")))
